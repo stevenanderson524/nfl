884e5e4b465e945aa1c7d0673bbd8ff081cd3eb1
stevenanderson524 <91501817+stevenanderson524@users.noreply.github.com>
2026-02-27 16:34:13 -0800

    Add loadDa function for DA live preview support
    Add async function to load daPreview script conditionally

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -187,3 +187,7 @@ async function loadPage() {
 }
 
 loadPage();
+(async function loadDa() {
+  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
+  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
+}());
